a6b6f31db971812afa118fd9c4162a3c9b0a1cd6
Matheus <matheusbarretoprogramador@gmail.com>
2024-02-21 23:59:19 -0300

    navbar menu opacity

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./styles/Navbar.module.css";
 import { Link } from "react-router-dom";
 import { IoMdMenu } from "react-icons/io";
+import { useSpring, animated } from "@react-spring/web";
 
 function Navbar() {
   const [showSubMenu, setShowSubMenu] = useState(false);
@@ -10,25 +11,35 @@ function Navbar() {
     setShowSubMenu(!showSubMenu);
   };
 
+  const props = useSpring({
+    reset: true,
+    from: {
+      opacity: 0,
+    },
+    to: {
+      opacity: showSubMenu ? 1 : 0,
+    },
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.menu} onClick={toggleSubMenu}>
         <IoMdMenu />
       </div>
       {showSubMenu && (
-        <div className={styles.submenu}>
+        <animated.div style={props} className={styles.submenu}>
           <Link to="/section1">SubSeção 1</Link>
           <Link to="/section2">SubSeção 2</Link>
           <Link to="/section3">SubSeção 3</Link>
           <Link to="/section4">SubSeção 4</Link>
           <Link to="/section5">SubSeção 5</Link>
-        </div>
+        </animated.div>
       )}
-      <Link to="/home">Home</Link>
-      <Link to="/home">Seção 1</Link>
-      <Link to="/home">Seção 2</Link>
-      <Link to="/home">Seção 3</Link>
-      <Link to="/home">Seção 4</Link>
+      <Link to="/">Seção 1</Link>
+      <Link to="/">Seção 2</Link>
+      <Link to="/">Seção 3</Link>
+      <Link to="/">Home</Link>
+      <Link to="/">Seção 4</Link>
       <Link to="/sobre">Sobre</Link>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,34 @@ import AnimatedPage from "../components/animations/AnimatedPage";
 function Home() {
   return (
     <AnimatedPage>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.contentLeft}>
+          Proin porta, enim in maximus tempor, lectus diam mattis metus, et
+          mattis sapien sapien ac nulla. Vestibulum ut nunc et metus viverra
+          faucibus. Proin et leo nunc. Nulla facilisi. Aenean nec venenatis
+          magna, pharetra consectetur nisi. Morbi a enim at augue convallis
+          volutpat et id nunc. Curabitur eu gravida massa. Nunc et elit euismod,
+          ultricies arcu at, mollis augue. Donec venenatis mauris quis turpis
+          aliquam dignissim. Aliquam erat volutpat. Nullam id malesuada est.
+        </div>
+        <div className={styles.contentCenter}>
+          Nam nisl leo, luctus ut cursus ut, tristique et tortor. Phasellus vel
+          congue magna. Quisque euismod lorem quis lectus malesuada volutpat. In
+          scelerisque justo id justo laoreet fermentum. Suspendisse nec posuere
+          est, eu molestie orci. Donec eu magna in nulla efficitur semper in non
+          arcu. Proin egestas dapibus faucibus. In vel vestibulum mauris.
+        </div>
+        <div className={styles.contentRight}>
+          {" "}
+          Proin porta, enim in maximus tempor, lectus diam mattis metus, et
+          mattis sapien sapien ac nulla. Vestibulum ut nunc et metus viverra
+          faucibus. Proin et leo nunc. Nulla facilisi. Aenean nec venenatis
+          magna, pharetra consectetur nisi. Morbi a enim at augue convallis
+          volutpat et id nunc. Curabitur eu gravida massa. Nunc et elit euismod,
+          ultricies arcu at, mollis augue. Donec venenatis mauris quis turpis
+          aliquam dignissim. Aliquam erat volutpat. Nullam id malesuada est.
+        </div>
+      </div>
     </AnimatedPage>
   );
 }
