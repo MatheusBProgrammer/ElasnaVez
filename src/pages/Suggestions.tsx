1f9db1de44171e5ac5fff6bf4607f4d7a3464901
MatheusBProgrammer <matheusbarretoprogramador@gmail.com>
2024-03-27 11:48:46 -0300

    suggestions 1.1

--- src/pages/Suggestions.tsx
+++ src/pages/Suggestions.tsx
@@ -4,53 +4,63 @@ import styles from "./styles/Suggestions.module.css";
 import SuggestionForm from "../components/layout/SuggestionForm";
 import AnimatedPage from "../components/animations/AnimatedPage";
 
-function Suggestions() {
-  const [showForm, setShowForm] = React.useState<boolean>(false);
+// Definindo o tipo para os itens de sugestões de livros
+interface BookSuggestion {
+  username: string;
+  bookname: string;
+  description: string;
+  type: string; // Opcional, caso nem todas as sugestões tenham uma data
+}
 
-  const bookSuggestions = [
-    {
-      username: "AnaSilva",
-      bookName: "Viagem ao Centro da Terra",
-      suggestionDate: "2024-03-20",
-      description:
-        "Uma aventura emocionante ao centro da Terra. Recomendo para quem ama exploração e mistérios.",
-    },
-    {
-      username: "CarlosFerreira",
-      bookName: "O Pequeno Príncipe",
-      suggestionDate: "2024-03-18",
-      description:
-        "Uma história tocante que nos faz refletir sobre o significado da vida e as relações humanas.",
-    },
-    {
-      username: "JuliaM",
-      bookName: "1984",
-      suggestionDate: "2024-03-15",
-      description:
-        "Uma distopia assustadora que é incrivelmente relevante no mundo de hoje. Faz pensar sobre liberdade e controle.",
-    },
-  ];
+function Suggestions() {
+  const [showForm, setShowForm] = React.useState(false);
+  // Informando ao TypeScript o tipo do estado
+  const [bookSuggestions, setBookSuggestions] = React.useState<
+    BookSuggestion[]
+  >([]);
+
+  React.useEffect(() => {
+    const fetchBookSuggestions = async () => {
+      try {
+        const response = await fetch("http://localhost:3333/list")
+          .then((resp) => resp.json())
+          .then((data) => {
+            setBookSuggestions(data);
+          })
+          .catch((e) => console.log(e));
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchBookSuggestions();
+  }, []); // O array vazio indica que este efeito roda apenas uma vez, quando o componente é montado.
 
   return (
     <div className={styles.page}>
       <h1>Sugestões de Livros</h1>
       {bookSuggestions.map((suggestion, index) => (
-        <div key={index} className={styles.suggestionItem}>
-          <h2>{suggestion.bookName}</h2>
-          <p className={styles.suggestionInfo}>
-            Sugerido por {suggestion.username}
-          </p>
-          <p className={styles.suggestionDescription}>
-            {suggestion.description}
-          </p>
+        <div
+          className={`${styles.suggestionsContainer} ${
+            index != 0 && styles.suggestionsContainerSecond
+          }`}
+          key={index}
+        >
+          <div className={styles.headerSuggestionsContainer}>
+            <h2>{suggestion.bookname}</h2>
+            <p>Sugerido por {suggestion.username}</p>
+          </div>
+
+          <p>{suggestion.description}</p>
         </div>
       ))}
-      <button onClick={() => setShowForm(!showForm)}>Dar uma sugestão</button>
-      {showForm && (
-        <AnimatedPage>
-          <SuggestionForm />
-        </AnimatedPage>
-      )}
+      <div className={styles.bottomSection}>
+        <button onClick={() => setShowForm(!showForm)}>Dar uma sugestão</button>
+        {showForm && (
+          <AnimatedPage>
+            <SuggestionForm />
+          </AnimatedPage>
+        )}
+      </div>
     </div>
   );
 }
